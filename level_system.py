812d4d1e0715a4fcb09c82fe1774e38b29e82fcf
xMissAlicex <42895934+xMissAlicex@users.noreply.github.com>
2018-09-20 16:57:28 +0200

    Update level_system.py

--- level_system.py
+++ level_system.py
@@ -1,14 +1,57 @@
 import discord
 import asyncio
 import json
+import pymysql
 from discord.ext import commands
 
 class Level_System:
     def __init__(self, client):
         self.client = client
 
-    def add_experience(self, users, user, exp):
-        users[user.id]['experience'] += exp
+    def create_user_database(self, user):
+        conn = pymysql.connect(host='sql7.freesqldatabase.com', user='sql7257339', password='yakm4fsd4T', db='sql7257339')
+        c = conn.cursor()
+        sql = "INSERT INTO `user_levels` VALUES ({}, '1', '0')".format(str(user.id))
+        c.execute(sql)
+        conn.commit()
+        conn.close()
+
+    def check_database(self, server, setting):
+        conn = pymysql.connect(host='sql7.freesqldatabase.com', user='sql7257339', password='yakm4fsd4T', db='sql7257339')
+        c = conn.cursor()
+        sql = "SELECT {} from `Server_Settings` WHERE serverid = {}".format(setting, str(server.id))
+        c.execute(sql)
+        conn.commit()
+        data = c.fetchone()
+        conn.close()
+        for row in data:
+            if row == 1:
+                return True
+            elif row == 0:
+                return False
+            else:
+                return row
+
+    def add_experience(self, user, exp):
+        conn = pymysql.connect(host='sql7.freesqldatabase.com', user='sql7257339', password='yakm4fsd4T', db='sql7257339')
+        c = conn.cursor()
+        sql = "SELECT exp from `user_levels` WHERE userid = {}".format(str(user.id))
+        c.execute(sql)
+        conn.commit()
+        data = c.fetchone()
+        conn.close()
+        for row in data:
+            current_exp = row
+        new_exp = int(current_exp) + int(exp)
+        conn = pymysql.connect(host='sql7.freesqldatabase.com', user='sql7257339', password='yakm4fsd4T', db='sql7257339')
+        c = conn.cursor()
+        sql = "UPDATE `user_levels` SET exp = %s where userid = %s"
+        t = (new_exp, str(user.id))
+        c.execute(sql, t)
+        conn.commit()
+        data = c.fetchone()
+        conn.close()
+        print("EXP added")
 
     def level_up(self, users, user, exp):
             experience = users[user.id]['experience']
@@ -20,38 +63,33 @@ class Level_System:
             else:
                 return False
 
-    def update_data(self, users, user):
-            if not user.id in users:
-                users[user.id] = {}
-                users[user.id]['experience'] = 0
-                users[user.id]['level'] = 1
 
     async def on_message(self, message):
         channel = message.channel
         user = message.author
         server = channel.server
-        with open('srv_settings.json', 'r') as f:
-            servers = json.load(f)
-            toggle = servers[server.id]["Level_System"]
-        if toggle == True:
-            if user.id != 481468291928293376:
-                with open('users.json', 'r') as f:
-                    users = json.load(f)
-
-                self.update_data(users, message.author)
-                self.add_experience(users, message.author, 5)
-                if self.level_up(users, message.author, message.channel):
-                    await self.client.send_message(channel, "{} You have gained a level!".format(user.mention))
-                with open('users.json', 'w') as f:
-                    json.dump(users, f)
+        if user.id != 485457192456290325:
+            toggle = self.check_database(server, "Level_System")
+            if toggle == True:
+                conn = pymysql.connect(host='sql7.freesqldatabase.com', user='sql7257339', password='yakm4fsd4T', db='sql7257339')
+                c = conn.cursor()
+                sql = "SELECT 'level' from `user_levels` WHERE userid = {}".format(str(user.id))
+                c.execute(sql)
+                conn.commit()
+                data = c.fetchone()
+                conn.close()
+                if data == None:
+                    self.create_user_database(user)
+                else:
+                    self.add_experience(message.author, 5)
+                    
 
 
     async def on_member_join(self, member):
         if member.id != 481468291928293376:
             with open('users.json', 'r') as f:
                 users = json.load(f)
-
-            await self.update_data(users, member)
+            self.update_data(users, member)
 
             with open('users.json', 'w') as f:
                 json.dump(users, f)
